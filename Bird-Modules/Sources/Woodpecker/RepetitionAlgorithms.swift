fdacc07d239658749b5e26dc107ab57c23b29405
caroltaus <carolinetaus@hotmail.com>
2022-08-22 17:53:55 -0300

    scheduler's initial implementation & CardModelTests

--- Bird-Modules/Sources/Woodpecker/RepetitionAlgorithms.swift
+++ Bird-Modules/Sources/Woodpecker/RepetitionAlgorithms.swift
@@ -12,15 +12,67 @@ import Models
 ///Woodpecker contains the algorithms used for Spaced Repetition
 public struct Woodpecker {
     
+    // id, woodkpecker, dueDate. 
+    public static func getTodaysCards(cards: [Card], config: SpacedRepetitionConfig ,currentDate: Date = Date()) throws -> (todayCards: [Card], toModify: [Card]) {
+        
+        if config.maxReviewingCards == 0 {
+            throw NSError()
+        } else if config.maxReviewingCards == 0 {
+            throw NSError()
+        }
+        
+        var learningCards: [Card] = cards
+            .filter({ !$0.woodpeckerCardInfo.isGraduated })
+            .shuffled()
+        
+        let learningHasBeenPresented = learningCards
+            .filter({ $0.woodpeckerCardInfo.hasBeenPresented })
+        
+        let learningHasntBeenPresented = learningCards
+            .filter({ !$0.woodpeckerCardInfo.hasBeenPresented })
+        
+        learningCards = Array((learningHasBeenPresented + learningHasntBeenPresented)
+            .prefix(config.maxLearningCards))
+        
+        var cal = Calendar(identifier: .gregorian)
+        guard let timezone = TimeZone(identifier: "UTC") else {
+            throw NSError()
+        }
+        cal.timeZone = timezone
+        
+        let reviewingDueTodayCards: [Card] = cards
+            .filter { card in
+                guard let dueDate = card.dueDate else {
+                    return false
+                }
+                let isDLessOrEqual = cal.dateComponents([.day], from: dueDate) == cal.dateComponents([.day], from: currentDate) || dueDate < currentDate
+                let isMLessOrEqual = cal.dateComponents([.month], from: dueDate) == cal.dateComponents([.month], from: currentDate) || dueDate < currentDate
+                let isYLessOrEqual = cal.dateComponents([.year], from: dueDate) == cal.dateComponents([.year], from: currentDate) || dueDate < currentDate
+                return card.woodpeckerCardInfo.isGraduated && isDLessOrEqual && isMLessOrEqual && isYLessOrEqual
+            }
+        
+        let todayReviewingCards = Array(reviewingDueTodayCards
+            .prefix(config.maxReviewingCards))
+        
+        let toModify = Array(reviewingDueTodayCards[config.maxReviewingCards...reviewingDueTodayCards.count])
+        
+        let todayCards = (todayReviewingCards + learningCards)
+            .shuffled()
+        
+        return (todayCards, toModify)
+        
+    }
+    
+    
     /**
      The stepper static method is the algorithm based on the Leitner system used for the learning stage
-      - Parameters:
-          - step: The step where the card currently is. From 0 to maximumStep.
-          - userGrade: The grade user gives to the difficulty of the card.
-          - numberOfSteps: The total number of steps. Must be greater than 1.
-      - Throws: 'WoodpeckerStepperErrors.negativeStep' if step is negative.
+     - Parameters:
+     - step: The step where the card currently is. From 0 to maximumStep.
+     - userGrade: The grade user gives to the difficulty of the card.
+     - numberOfSteps: The total number of steps. Must be greater than 1.
+     - Throws: 'WoodpeckerStepperErrors.negativeStep' if step is negative.
      'WoodpeckerStepperErrors.notEnoughSteps' if maximumStep is lower than 1.
-      - Returns: The case that determines the cards destiny.
+     - Returns: The case that determines the cards destiny.
      */
     public static func stepper(step: Int, userGrade: UserGrade, numberOfSteps: Int) throws -> CardDestiny {
         
@@ -43,12 +95,12 @@ public struct Woodpecker {
     
     /**
      The wpSm2 static method contains the algorithm based on Super Memo 2 algorithm used for scheduling cards in the reviewing stage.
-    - Parameters:
-        - card: The card that is going to be scheduled.
-        - userGrade: The grade user gives to the difficulty of the card.
-    - Throws: 'WoodpeckerSm2Errors.isNotGraduated' if the card is not graduated.
+     - Parameters:
+     - card: The card that is going to be scheduled.
+     - userGrade: The grade user gives to the difficulty of the card.
+     - Throws: 'WoodpeckerSm2Errors.isNotGraduated' if the card is not graduated.
      'WoodpeckerSm2Errors.stepNot0' if the card has a step different from 0.
-    - Returns: The modified card. It might modify its Interval, isGraduated, streak, and easeFactor.
+     - Returns: The modified card. It might modify its Interval, isGraduated, streak, and easeFactor.
      */
     static func wpSm2(_ card: WoodpeckerCardInfo, userGrade: UserGrade) throws -> WoodpeckerCardInfo {
         let streak: Int = card.streak
---
+++ Bird-Modules/Tests/WoodpeckerTests/WpGetTodayTests.swift
@@ -0,0 +1,58 @@
+//
+//  WpGetTodayTests.swift
+//  
+//
+//  Created by Caroline Taus on 22/08/22.
+//
+
+import XCTest
+@testable import Woodpecker
+import Models
+
+class WpGetTodayTests: XCTestCase {
+    let today: Date = Date(timeIntervalSince1970: 0)
+    
+    // Learning stage with 40 cards never been presented and 0 that have been presented
+    func testLearning40N0HB() {
+        let cards = [Card(id: UUID(),
+                          front: "",
+                          back: "",
+                          datesLogs: DateLogs(lastAccess: Date(),
+                                              lastEdit: Date(),
+                                              createdAt: Date()),
+                          deckID: UUID(),
+                          woodpeckerCardInfo: WoodpeckerCardInfo(step: 0,
+                                                                 isGraduated: false,
+                                                                 easeFactor: 2.5,
+                                                                 streak: 0,
+                                                                 interval: 0,
+                                                                 hasBeenPresented: false),
+                          history: []),
+                     Card(id: UUID(),
+                          front: "",
+                          back: "",
+                          datesLogs: DateLogs(lastAccess: Date(),
+                                              lastEdit: Date(),
+                                              createdAt: Date()),
+                          deckID: UUID(),
+                          woodpeckerCardInfo: WoodpeckerCardInfo(step: 0,
+                                                                 isGraduated: false,
+                                                                 easeFactor: 2.5, streak: 0,
+                                                                 interval: 0,
+                                                                 hasBeenPresented: false),
+                          history: []),
+                     
+        ]
+        let resultCards = try! Woodpecker.getTodaysCards(cards: [], config: SpacedRepetitionConfig(maxLearningCards: 4, maxReviewingCards: 4), currentDate: today)
+        
+    }
+    
+    
+    let LHB: [Card] = []
+    let LNHB: [Card] = []
+    
+    let RDT: [Card] = []
+    let RDY: [Card] = []
+    let RDL: [Card]
+
+}
