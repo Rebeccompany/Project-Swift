df4e83d14fe216f0d4a8117591c24115654e5e91
Nathalia Papst <nathaliapapst@gmail.com>
2022-09-01 17:49:24 -0300

    UIColor hex

--- Bird-Modules/Sources/HummingBird/HBColors.swift
+++ Bird-Modules/Sources/HummingBird/HBColors.swift
@@ -8,33 +8,60 @@
 import Foundation
 import SwiftUI
 
-struct HBColor {
-    var light: Color
-    var dark: Color
+enum HBColor {
     
-    func color(for scheme: ColorScheme) -> Color {
-        switch scheme {
-        case .light:
-            return light
-        case .dark:
-            return dark
-        @unknown default:
-            return light
-        }
+    static var collectionRed: Color {
+        Color(
+            light: "#DC443B",
+            dark: "#DC4833")
+    }
+    
+    static var collectionOrange: Color {
+        Color(
+            light: "#F37012",
+            dark: "#FF934F")
+    }
+    
+    static var collectionYellow: Color {
+        Color(
+            light: "#F4BF00",
+            dark: "#E5A832")
+    }
+    
+    static var collectionGreen: Color {
+        Color(
+            light: "#038858",
+            dark: "#63B995")
+    }
+    
+    static var collectionLightBlue: Color {
+        Color(
+            light: "#008DC6",
+            dark: "#009DDC")
+    }
+    
+    static var collectionDarkPurple: Color {
+        Color(
+            light: "#6665DD",
+            dark: "#6665DD")
+    }
+    
+    static var collectionLightPurple: Color {
+        Color(
+            light: "#9E74CF",
+            dark: "#B388E5")
+    }
+    
+    static var collectionStrongPink: Color {
+        Color(
+            light: "#DB5DBF",
+            dark: "#C75BAB")
+    }
+    
+    static var collectionWeakPink: Color {
+        Color(
+            light: "#E7667D",
+            dark: "#C75BAB")
     }
 }
 
-extension HBColor {
-    static var collectionRed: HBColor {
-        HBColor(
-            light: Color(hex: "#DC443B") ?? .red,
-            dark: Color(hex: "#DC4833") ?? .red
-        )
-    }
-    
-    static var collectionOrange: HBColor {
-        HBColor(
-            light: Color(hex: "#F37012") ?? .orange,
-            dark: Color(hex: "#FF934F") ?? .red)
-    }
-}
