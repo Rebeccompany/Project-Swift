fe0fe10cd742d07ef67aefbfa59e0ca6a596c5ea
Gabriel Ferreira de Carvalho <60374568+gbrlCM@users.noreply.github.com>
2022-09-02 16:34:50 -0300

    dificulty button (#57)

--- Bird-Modules/Sources/HummingBird/HBColors.swift
+++ Bird-Modules/Sources/HummingBird/HBColors.swift
@@ -8,9 +8,9 @@
 import Foundation
 import SwiftUI
 
-enum HBColor {
+public enum HBColor {
     
-    static var collectionRed: Color {
+    public static var collectionRed: Color {
         Color(
             light: "#DC443B",
             dark: "#DC4833")
@@ -22,112 +22,126 @@ enum HBColor {
             dark: "#FF934F")
     }
     
-    static var collectionYellow: Color {
+    public static var collectionYellow: Color {
         Color(
             light: "#F4BF00",
             dark: "#E5A832")
     }
     
-    static var collectionGreen: Color {
+    public static var collectionGreen: Color {
         Color(
             light: "#038858",
             dark: "#63B995")
     }
     
-    static var collectionLightBlue: Color {
+    public static var collectionLightBlue: Color {
         Color(
             light: "#008DC6",
             dark: "#009DDC")
     }
     
-    static var collectionDarkPurple: Color {
+    public static var collectionDarkPurple: Color {
         Color(
             light: "#6665DD",
             dark: "#6665DD")
     }
     
-    static var collectionLightPurple: Color {
+    public static var collectionLightPurple: Color {
         Color(
             light: "#9E74CF",
             dark: "#B388E5")
     }
     
-    static var collectionPink: Color {
+    public static var collectionPink: Color {
         Color(
             light: "#DB5DBF",
             dark: "#C75BAB")
     }
     
-    static var collectionOtherPink: Color {
+    public static var collectionOtherPink: Color {
         Color(
             light: "#E7667D",
             dark: "#C64573")
     }
     
-    static var collectionBeigeBrown: Color {
+    public static var collectionBeigeBrown: Color {
         Color(
             light: "#C89085",
             dark: "#E3B7B0")
     }
     
-    static var collectionGray: Color {
+    public static var collectionGray: Color {
         Color(
             light: "#929292",
             dark: "#8B868D")
     }
     
-    static var collectionDarkBlue: Color {
+    public static var collectionDarkBlue: Color {
         Color(
             light: "#254A93",
             dark: "#354F83")
     }
     
-    static var actionColor: Color {
+    public static var actionColor: Color {
         Color(
             light: "#1394F7",
             dark: "#21B3C9")
     }
     
-    static var secondaryPurpleColor: Color {
+    public static var secondaryPurpleColor: Color {
         Color(
             light: "#4C3DF5",
             dark: "#7A6EF7")
     }
     
-    static var secondaryOrangeYellowColor: Color {
+    public static var secondaryOrangeYellowColor: Color {
         Color(
             light: "#FE4C0E",
             dark: "#FEE440")
     }
     
-    static var secondaryGreenColor: Color {
+    public static var secondaryGreenColor: Color {
         Color(
             light: "#05A287",
             dark: "#B4E40E")
     }
     
-    static var veryHardColor: Color {
+    public static var veryHardColor: Color {
         Color(
             light: "#FF6961",
             dark: "#FF6961")
     }
 
-    static var hardColor: Color {
+    public static var hardColor: Color {
         Color(
             light: "#FFB340",
             dark: "#FFB340")
     }
     
-    static var easyColor: Color {
+    public static var easyColor: Color {
         Color(
             light: "#409CFF",
             dark: "#409CFF")
     }
     
-    static var veryeasyColor: Color {
+    public static var veryeasyColor: Color {
         Color(
             light: "#30DB5B",
             dark: "#30DB5B")
+    }
+    
+    public static var primaryBackground: Color {
+        Color(
+            light: "#F2F2F7",
+            dark: "#191F2D"
+        )
+    }
+    
+    public static var secondaryBackground: Color {
+        Color(
+            light: "#FEFEFE",
+            dark: "#2B354C"
+        )
     }
 
 }
