6d704562221357d186bdb0f71a37c92639e41dda
Marcos Chevis <marcoschevism@gmail.com>
2022-09-05 16:25:45 -0300

    session struct

---
+++ Bird-Modules/Sources/Models/Session.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marcos Chevis on 05/09/22.
+//
+
+import Foundation
+
+public struct Session {
+    public var cardIds: [UUID]
+    public var date: Date
+}
