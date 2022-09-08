14b1d04afe37e9136e3c2b788691e9a9ebc58c25
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2022-09-08 14:17:59 -0300

    Update DeckView.swift

---
+++ Bird-Modules/Sources/DeckFeature/DeckView.swift
@@ -0,0 +1,15 @@
+//
+//  DeckView.swift
+//  
+//
+//  Created by Marcos Chevis on 30/08/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct DeckView: View {
+    var body: some View {
+        Text("oi")
+    }
+}
