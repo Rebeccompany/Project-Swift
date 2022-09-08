97f768e66559daabab98ded69fdbe6ae60adad53
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2022-09-08 14:39:58 -0300

    started deck view

--- Bird-Modules/Sources/DeckFeature/DeckView.swift
+++ Bird-Modules/Sources/DeckFeature/DeckView.swift
@@ -7,9 +7,30 @@
 
 import Foundation
 import SwiftUI
+import Models
 
 struct DeckView: View {
+    
     var body: some View {
-        Text("oi")
+        List {
+            
+        }
+        .navigationTitle(Text("oi").foregroundColor(.red))
+    }
+}
+
+final class DeckViewModel: ObservableObject {
+    @Published var deck: Deck
+    
+    init(deck: Deck) {
+        self.deck = deck
+    }
+}
+
+struct DeckView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DeckView()
+        }
     }
 }
