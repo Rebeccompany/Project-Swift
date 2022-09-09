3657c54ba4a22145de56559896ee7011e9556fef
caroltaus <carolinetaus@hotmail.com>
2022-09-09 14:15:02 -0300

    accessibilidade

--- Bird-Modules/Sources/NewCollectionFeature/NewCollectionView.swift
+++ Bird-Modules/Sources/NewCollectionFeature/NewCollectionView.swift
@@ -41,7 +41,7 @@ public struct NewCollectionView: View {
                         } label: {
                             HBColor.getHBColrFromCollectionColor(color)
                         }
-//                        .accessibility(label: Text(CollectionColor.getColorString(color)))
+                        .accessibility(label: Text(CollectionColor.getColorString(color)))
                         .buttonStyle(ColorIconButtonStyle(isSelected: viewModel.currentSelectedColor == color ? true : false))
                         .frame(width: 45, height: 45)
                     }
