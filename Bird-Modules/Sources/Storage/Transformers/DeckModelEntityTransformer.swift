6e503f58617865703b2904718090f0e94965e238
Marcos Chevis <marcoschevism@gmail.com>
2022-09-20 18:04:40 -0300

    implemented cloudKit

--- Bird-Modules/Sources/Storage/Transformers/DeckModelEntityTransformer.swift
+++ Bird-Modules/Sources/Storage/Transformers/DeckModelEntityTransformer.swift
@@ -41,15 +41,15 @@ struct DeckModelEntityTransformer: ModelEntityTransformer {
             let lastAccess = entity.lastAccess,
             let createdAt = entity.createdAt,
             let lastEdit = entity.lastEdit,
-            let collection = entity.collection,
             let cards = entity.cards?.allObjects as? [CardEntity]
         else { return nil }
+        let collection = entity.collection
         
         let maxLearningCards = entity.maxLearningCards
         let maxReviewingCards = entity.maxReviewingCards
         let spacedRepetitionConfig = SpacedRepetitionConfig(maxLearningCards: Int(maxLearningCards), maxReviewingCards: Int(maxReviewingCards), numberOfSteps: Int(entity.numberOfSteps))
         let dateLogs = DateLogs(lastAccess: lastAccess, lastEdit: lastEdit, createdAt: createdAt)
-        let collectionIds = collection.id
+        let collectionIds = collection?.id
         let cardsIds = cards.compactMap(\.id)
         
         return Deck(
