7460e84d5dde9d5ca94661e0fe6849b6fa32b2e5
Marcos Chevis <marcoschevism@gmail.com>
2022-09-20 15:17:26 -0300

    fixed conflict

--- Bird-Modules/Sources/NewDeckFeature/NewDeckViewModel.swift
+++ Bird-Modules/Sources/NewDeckFeature/NewDeckViewModel.swift
@@ -80,7 +80,7 @@ public class NewDeckViewModel: ObservableObject {
                     icon: selectedIcon.rawValue.description,
                     color: selectedColor,
                     datesLogs: DateLogs(lastAccess: dateHandler.today, lastEdit: dateHandler.today, createdAt: dateHandler.today),
-                    collectionsIds: collectionId,
+                    collectionId: collectionId,
                     cardsIds: [],
                     spacedRepetitionConfig: SpacedRepetitionConfig()),
             cards: [])
