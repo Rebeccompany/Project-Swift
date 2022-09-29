2c69b9b303dcb37b2ef763789119db3625dfbfbd
Gabriel Ferreira de Carvalho <60374568+gbrlCM@users.noreply.github.com>
2022-09-28 22:19:27 -0300

    changed table so it can react to size class (#244)

--- Bird-Modules/Sources/AppFeature/Detail/DeckTableView.swift
+++ Bird-Modules/Sources/AppFeature/Detail/DeckTableView.swift
@@ -11,6 +11,7 @@ import HummingBird
 
 struct DeckTableView: View {
     @EnvironmentObject private var viewModel: ContentViewModel
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     private var sortedDecks: [Deck] {
         viewModel.decks.sorted(using: viewModel.sortOrder)
@@ -20,32 +21,34 @@ struct DeckTableView: View {
         if viewModel.decks.isEmpty {
             EmptyStateView(component: .deck)
         } else {
+            content
+        }
+    }
+    
+    @ViewBuilder
+    private var content: some View {
+        if horizontalSizeClass == .compact {
+            list
+        } else {
             table
         }
+    }
+    
+    @ViewBuilder
+    private var list: some View {
+        List(sortedDecks) { deck in
+            NavigationLink(value: StudyRoute.deck(deck)) {
+                cell(for: deck)
+            }
+        }
+        .listStyle(.plain)
     }
     
     @ViewBuilder
     private var table: some View {
         Table(sortedDecks, selection: $viewModel.selection, sortOrder: $viewModel.sortOrder) {
             TableColumn("Nome", value: \.name) { deck in
-                NavigationLink(value: deck) {
-                    HStack {
-                        Image(systemName: deck.icon)
-                            .foregroundColor(HBColor.color(for: deck.color))
-                            .background(
-                                Circle()
-                                    .fill(HBColor.color(for: deck.color).opacity(0.2))
-                                
-                                    .frame(width: 35, height: 35)
-                                    
-                            )
-                            .frame(width: 35, height: 35)
-                            .padding(.trailing, 8)
-                        Text(deck.name)
-                            .foregroundColor(.primary)
-                    }
-                    .padding(.leading, 4)
-                }
+                cell(for: deck)
             }
             TableColumn("Flashcards", value: \.cardCount) { deck in
                 Text("\(deck.cardCount) flashcards")
@@ -53,7 +56,35 @@ struct DeckTableView: View {
             TableColumn("Último Acesso", value: \.datesLogs.lastAccess) { deck in
                 Text(deck.datesLogs.lastAccess, style: .date)
             }
+            TableColumn("Acessar") { deck in
+                NavigationLink(value: StudyRoute.deck(deck)) {
+                    Text("Abrir")
+                }
+                .tint(HBColor.color(for: deck.color))
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.capsule)
+            }.width(90)
         }
         .animation(.linear, value: viewModel.sortOrder)
+    }
+    
+    @ViewBuilder
+    private func cell(for deck: Deck) -> some View {
+        HStack {
+            Image(systemName: deck.icon)
+                .foregroundColor(HBColor.color(for: deck.color))
+                .background(
+                    Circle()
+                        .fill(HBColor.color(for: deck.color).opacity(0.2))
+                    
+                        .frame(width: 35, height: 35)
+                        
+                )
+                .frame(width: 35, height: 35)
+                .padding(.trailing, 8)
+            Text(deck.name)
+                .foregroundColor(.primary)
+        }
+        .padding(.leading, 4)
     }
 }
