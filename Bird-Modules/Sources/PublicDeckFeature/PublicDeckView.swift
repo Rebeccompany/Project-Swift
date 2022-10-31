1ffc069ce3673130cf55ab7c755f0defd28e6176
Rebecca Mello <rebecca.mello@me.com>
2022-10-31 16:02:17 -0300

    beginning of view model

--- Bird-Modules/Sources/PublicDeckFeature/PublicDeckView.swift
+++ Bird-Modules/Sources/PublicDeckFeature/PublicDeckView.swift
@@ -11,62 +11,70 @@ import HummingBird
 struct PublicDeckView: View {
     @Binding var description: String
     @StateObject private var viewModel: PublicDeckViewModel = PublicDeckViewModel()
-
+    @State private var shouldDisplayNewFlashcard: Bool = false
+    
     
     var body: some View {
-        VStack {
-            HeaderPublicDeckView()
-            HStack {
-                Button {
-                    
-                } label: {
-                    Image(systemName: "square.and.arrow.down")
-                    Text("Download")
-                }
-                .bold()
-                .buttonStyle(.borderedProminent)
-                .tint(HBColor.actionColor.opacity(0.15))
-                .foregroundColor(HBColor.actionColor)
-                .padding(.bottom)
-                
-                Button {
-                    
-                } label: {
-                    Image(systemName: "square.and.arrow.up")
-                    Text("Compartilhar")
-                }
-                .bold()
-                .buttonStyle(.borderedProminent)
-                .tint(HBColor.actionColor.opacity(0.15))
-                .foregroundColor(HBColor.actionColor)
-                .padding(.bottom)
-            }
-            
-            TextEditor(text: $description)
-                .foregroundColor(.black)
-                .padding([.horizontal, .bottom], 16)
-                .scrollContentBackground(.hidden)
-                .background(.white)
-                .frame(height: 300)
-                .clipShape(
-                    RoundedRectangle(cornerRadius: 16)
-                )
-            
-            Text("Flashcards")
-                .font(.title3)
-                .bold()
-                .padding(.leading)
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 160, maximum: 180), spacing: 12, alignment: .top)], spacing: 12) {
-                ForEach(viewModel.cardsSearched) { card in
-                    FlashcardCell(card: card) {
-                        shouldDisplayNewFlashcard = true
+        ScrollView {
+            VStack {
+                VStack {
+                    HeaderPublicDeckView()
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "square.and.arrow.down")
+                            Text("Download")
+                        }
+                        .bold()
+                        .buttonStyle(.borderedProminent)
+                        .tint(HBColor.actionColor.opacity(0.15))
+                        .foregroundColor(HBColor.actionColor)
+                        .padding(.bottom)
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "square.and.arrow.up")
+                            Text("Compartilhar")
+                        }
+                        .bold()
+                        .buttonStyle(.borderedProminent)
+                        .tint(HBColor.actionColor.opacity(0.15))
+                        .foregroundColor(HBColor.actionColor)
+                        .padding(.bottom)
                     }
+                    
+                    TextEditor(text: $description)
+                        .foregroundColor(.black)
+                        .padding([.horizontal, .bottom], 16)
+                        .scrollContentBackground(.hidden)
+                        .background(.white)
+                        .frame(height: 300)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 16)
+                        )
+                        .padding(.bottom)
                 }
-                .listRowSeparator(.hidden)
+                
+                LazyVStack(alignment: .leading){
+                    Text("Flashcards")
+                        .font(.title3)
+                        .bold()
+                }
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160, maximum: 180), spacing: 12, alignment: .top)], spacing: 12) {
+                    ForEach(viewModel.cards) { card in
+                        FlashcardCell(card: card) {
+                            shouldDisplayNewFlashcard = true
+                        }
+                    }
+                    .listRowSeparator(.hidden)
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
+            .padding()
         }
-        .padding()
         .viewBackgroundColor(HBColor.primaryBackground)
     }
 }
---
+++ Bird-Modules/Sources/Puffins/Secrets/ApiKeys.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//
+//
+//  Created by Rebecca Mello on 28/10/22.
+//
+
+import Foundation
+
+enum ApiKeys {
+    static let baseRoute: String = "crow-dev.eba-udf2azmf.sa-east-1.elasticbeanstalk.com"
+}
