8ce4daf2d5cbec84ae6b18a23411f6bfd9c7117f
Nathalia Papst <nathaliapapst@gmail.com>
2022-11-18 18:09:03 -0300

    Mudar ícones dos botões de estudo

--- Bird-Modules/Sources/StudyFeature/DifficultyStep.swift
+++ Bird-Modules/Sources/StudyFeature/DifficultyStep.swift
@@ -44,13 +44,13 @@ struct DifficultyButtonContent {
     static func getbuttonContent(for userGrade: UserGrade ) -> DifficultyButtonContent {
         switch userGrade {
         case .wrongHard:
-            return DifficultyButtonContent(image: "xmark.seal", label: NSLocalizedString("muito_dificil", bundle: .module, comment: ""), color: HBColor.veryHardColor)
+            return DifficultyButtonContent(image: "hand.raised.slash", label: NSLocalizedString("muito_dificil", bundle: .module, comment: ""), color: HBColor.veryHardColor)
         case .wrong:
-            return DifficultyButtonContent(image: "xmark", label: NSLocalizedString("dificil", bundle: .module, comment: ""), color: HBColor.hardColor)
+            return DifficultyButtonContent(image: "hand.thumbsdown", label: NSLocalizedString("dificil", bundle: .module, comment: ""), color: HBColor.hardColor)
         case .correct:
-            return DifficultyButtonContent(image: "checkmark", label: NSLocalizedString("facil", bundle: .module, comment: ""), color: HBColor.easyColor)
+            return DifficultyButtonContent(image: "hand.thumbsup", label: NSLocalizedString("facil", bundle: .module, comment: ""), color: HBColor.easyColor)
         case .correctEasy:
-            return DifficultyButtonContent(image: "checkmark.seal", label: NSLocalizedString("muito_facil", bundle: .module, comment: ""), color: HBColor.veryEasyColor)
+            return DifficultyButtonContent(image: "hands.sparkles", label: NSLocalizedString("muito_facil", bundle: .module, comment: ""), color: HBColor.veryEasyColor)
         }
     }
 }
