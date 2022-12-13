5f36cc109c051c070c4cfbe69cd34e69c4583b34
claudia1402 <claudiafiorentino2002@gmail.com>
2022-12-13 17:59:50 -0300

    creating medium widget view

--- Bird-Modules/Sources/Kiwi/WidgetView.swift
+++ Bird-Modules/Sources/Kiwi/WidgetView.swift
@@ -20,8 +20,9 @@ struct WidgetView: View {
             smallWidget
             .viewBackgroundColor(HBColor.primaryBackground)
             
-        case .systemMedium:
-            
+        default:
+            mediumWidget
+            .viewBackgroundColor(HBColor.primaryBackground)
         }
         
     }
@@ -44,6 +45,78 @@ struct WidgetView: View {
                 HBImages.spixiiDrinkingWater
                     .resizable()
                     .frame(width: 100, height: 80)
+                
+            }
+            else {
+                ForEach(baralhos.prefix(2)) { baralho in
+                    HStack {
+                        Divider()
+                            .frame(width: 5, height: 50)
+                            .overlay(HBColor.collectionDarkPurple)
+                            .cornerRadius(30)
+                        ZStack {
+                            Circle()
+                                .frame(width: 35)
+                                .foregroundColor(HBColor.collectionDarkPurple)
+                                .brightness(0.3)
+                            Image(systemName: "\(baralho.icon)")
+                                .foregroundColor(HBColor.collectionDarkPurple)
+                        }
+                        VStack {
+                            Text("\(baralho.name)")
+                                .foregroundColor(HBColor.collectionTextColor)
+                                .fontWeight(.semibold)
+                            
+                            Text("\(baralho.cardsIds.count) cartas para hoje")
+                                .foregroundColor(HBColor.collectionGray)
+                                .fontWeight(.light)
+                        }
+                    }
+                }
+                
+                if baralhos.count > 2 {
+                    HStack {
+                        Image(systemName: "ellipsis.circle")
+                        Text("Mais \(baralhos.count - 2) baralhos")
+                            .foregroundColor(HBColor.collectionTextColor)
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    @ViewBuilder
+    private var mediumWidget: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Baralhos Diários")
+                    .lineLimit(nil)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(HBColor.collectionTextColor)
+                Spacer()
+                widgetDateBuilder(date: Date())
+            }
+            
+            if baralhos.isEmpty {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Piu! Dia de Descanso!")
+                            .lineLimit(nil)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(HBColor.collectionOrange)
+                        Text("Sem estudos por hoje! Até logo!")
+                            .lineLimit(nil)
+                            .font(.title3)
+                            .fontWeight(.light)
+                            .foregroundColor(HBColor.collectionGray)
+                    }
+                    HBImages.spixiiBeach
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
                 
             }
             else {
